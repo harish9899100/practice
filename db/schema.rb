ed010fae229eefd22047981a2627db552760d7a6
harish khileri <harishkhileri98@gmail.com>
2026-04-28 15:22:34 +0530

    remove product from schema this is another branchs code

--- db/schema.rb
+++ db/schema.rb
@@ -79,17 +79,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_28_081712) do
     t.index ["department_id"], name: "index_positions_on_department_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.boolean "active"
-    t.datetime "created_at", null: false
-    t.boolean "featured"
-    t.string "name"
-    t.integer "price"
-    t.string "sku"
-    t.integer "stock_count"
-    t.datetime "updated_at", null: false
-    t.index ["sku"], name: "index_products_on_sku", unique: true
-  end
+  # create_table "products", force: :cascade do |t|
+  #   t.boolean "active"
+  #   t.datetime "created_at", null: false
+  #   t.boolean "featured"
+  #   t.string "name"
+  #   t.integer "price"
+  #   t.string "sku"
+  #   t.integer "stock_count"
+  #   t.datetime "updated_at", null: false
+  #   t.index ["sku"], name: "index_products_on_sku", unique: true
+  # end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false

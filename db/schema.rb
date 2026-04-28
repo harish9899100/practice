fb50f928adaf4faa9fcf282a4e54c21741ae785d
harish khileri <harishkhileri98@gmail.com>
2026-04-28 12:29:44 +0530

    create department model and associate with organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_28_063828) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_28_065826) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.integer "organization_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_departments_on_organization_id"
   end
 
   create_table "inventory_logs", force: :cascade do |t|
@@ -45,4 +54,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_28_063828) do
     t.datetime "updated_at", null: false
     t.index ["sku"], name: "index_products_on_sku", unique: true
   end
+
+  add_foreign_key "departments", "organizations"
 end

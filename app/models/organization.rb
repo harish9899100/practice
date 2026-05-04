8d04482a94a5b90451ef73d7fbc92bfe1e3fef28
harish khileri <harishkhileri98@gmail.com>
2026-05-04 10:36:34 +0530

    add organization at admin penal

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,9 @@
 class Organization < ApplicationRecord
   has_many :departments
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "updated_at", "department_id"]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    ["departmes"]
+  end
 end
